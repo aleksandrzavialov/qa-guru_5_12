6cfa9333ea3550ebd0589fed04348e3504306351
Aleksandr Zavialov <aleksandr.zavialov@grse.de>
2023-05-18 07:21:40 +0300

    Return browser test and use Allure attachments

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,17 +1,30 @@
 import datetime
+import allure
+from selene import browser
 
 from demoqa_tests.pages.registration_page import RegistrationPage
 from demoqa_tests.data.users import User, UserHobby, UserGender
+from utils import attach
 
 
-#def test_form_filling(browser_actions):
-def test_form_filling():
-    pass
-    # registration_page = RegistrationPage()
-    # tested_user = User(first_name='Ivan', last_name='Petrov', email='ivanpetrov@mail.ru', gender=UserGender.Male.name,
-    #                    mobile='8999999999', date_of_birth=datetime.date(1989, 11, 19), subjects=['Maths', 'Hindi'],
-    #                    hobby=UserHobby.Reading.name, picture='Capibara.jpg', address='Pushkina Street, Kolotushkina house',
-    #                    state='Uttar Pradesh', city='Merrut')
-    # registration_page.open()
-    # registration_page.register(tested_user)
-    # registration_page.should_have_registered(tested_user)
+@allure.title('Successful registration in demo QA')
+def test_form_filling(browser_actions):
+    registration_page = RegistrationPage()
+    tested_user = User(first_name='Ivan', last_name='Petrov', email='ivanpetrov@mail.ru', gender=UserGender.Male.name,
+                       mobile='8999999999', date_of_birth=datetime.date(1989, 11, 19), subjects=['Maths', 'Hindi'],
+                       hobby=UserHobby.Reading.name, picture='Capibara.jpg',
+                       address='Pushkina Street, Kolotushkina house',
+                       state='Uttar Pradesh', city='Merrut')
+
+    with allure.step('Open demo QA page'):
+        registration_page.open()
+
+    with allure.step('Fill registration information'):
+        registration_page.register(tested_user)
+
+    with allure.step("Check registration results"):
+        registration_page.should_have_registered(tested_user)
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
